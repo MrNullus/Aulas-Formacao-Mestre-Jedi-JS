89b867a3fcb1323607bff622747c1cc561fe5ef0
Dev-gustavo-henrique <gustavojs417@gmail.com>
2021-10-23 16:59:48 -0300

    Create js.js

---
+++ 04 - primeira missão estelar JS/js/js.js
@@ -0,0 +1,11 @@
+// declaração de variaveis
+var missao = "Missão Esterlar JS em execução !!!";
+
+function eventoOnload(){
+    console.log("Evento onload disparado");
+}
+
+function eventoOnclick(){
+    console.log("Missão Esterlar JS realizada com sucesso !!!");
+}
+
