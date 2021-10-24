a721a147bc5e2055377be3232142b14db79349a0
Dev-gustavo-henrique <gustavojs417@gmail.com>
2021-10-24 16:40:09 -0300

    Create number.js

---
+++ 06 - Tipos de dados no JS/js/number.js
@@ -0,0 +1,37 @@
+var idade = 28;
+console.log(idade);
+
+console.log(typeof idade);
+
+var precoFeijao = 2.89;
+ 
+console.log(precoFeijao);
+console.log(typeof precoFeijao);
+
+var valor = 23 + 33;
+console.log(valor);
+
+//* Vai transformar a string "23" em numero
+var valor = +"23" + 33;
+console.log(valor);
+
+var numero = -38;
+console.log(numero);
+console.log(numero + valor);
+
+// Maior valor suportado pelo JS
+console.log(Number.MAX_VALUE);
+// Menor valor suportado pelo JS
+console.log(Number.MIN_VALUE);
+
+console.log(valor);
+// Retora numa string com o número na base 10 (decimal)
+console.log(valor.toString(10));
+// Retorna numa string com o número na base 2 (binario)
+console.log(valor.toString(2));
+// Retorna numa string com o número na base 8 (octa)
+console.log(valor.toString(8));
+// Retorna numa string com o número na base 16 (hexa)
+console.log(valor.toString(16));
+
+
