70b06c94e9c0e128c025de5801ea3c47022604c1
Dev-gustavo-henrique <gustavojs417@gmail.com>
2021-10-24 16:39:58 -0300

    Create string.js

---
+++ 06 - Tipos de dados no JS/js/string.js
@@ -0,0 +1,18 @@
+var nome = "Prof. Madson";
+var sobreNome = 'Aguiar Rodrigues';
+var nomeCompleto = nome + " " + sobreNome;
+var vazia = "texto \
+texto \
+textto";  //Essa barra invertida  serve para indicar que tem quebra de linha
+
+console.log("Nome Completo: " + nomeCompleto);
+
+var stringVar = "string text";
+var numeroVar = 10;
+
+//O operador typeof vai retornar o tipo de dado
+console.log("Tipo 1: " + typeof stringVar);
+console.log("Tipo 2: " + typeof numeroVar);
+
+//O método .length vai retornar o tamanho da string
+console.log("Tamanho: " + stringVar.length);
