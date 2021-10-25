a1975a89be4b0d4e350860261587e932be121ea8
Dev-gustavo-henrique <gustavojs417@gmail.com>
2021-10-25 02:29:10 -0300

    Create function.js

---
+++ 06 - Tipos de dados no JS/js/function.js
@@ -0,0 +1,11 @@
+function exibirNoConsole(mensagem){
+    console.log(mensagem);
+}
+
+exibirNoConsole("Gustavo");
+
+function soma(val1, val2){
+    return val1 + val2;
+}
+
+console.log(soma(20, 22));
