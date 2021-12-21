5ba5066b67f788c23647414218bdbd7d129020cd
Dev-gustavo-henrique <gustavojs417@gmail.com>
2021-12-21 19:48:36 -0300

    Create aritmeticos.js

---
+++ 11 - Operadores/ex/aritmeticos.js
@@ -0,0 +1,16 @@
+console.log(" + Operadores Aritmenticos + ");
+
+let soma = 35 + 12;;
+console.log("Soma: ", soma);
+
+let subtracao = 90 - 39;
+console.log("Subtração: ", subtracao);
+
+let multiplicacao = 23 * 90;
+console.log("Multiplicação: ", multiplicacao);
+
+let divisao = 56/12;
+console.log("Divisão: ", divisao);
+
+let exponenciacao = 12**8;
+console.log("Exponenciação: ", exponenciacao);
