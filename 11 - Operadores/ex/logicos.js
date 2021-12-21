68a6ecd88630c895117178914dd7025f178992e3
Dev-gustavo-henrique <gustavojs417@gmail.com>
2021-12-21 19:48:23 -0300

    Create logicos.js

---
+++ 11 - Operadores/ex/logicos.js
@@ -0,0 +1,21 @@
+//* && (AND)
+var result = (10 == 10) && (5 == 5);
+var result2 = true &&  true;
+var result3 = true && 'true';
+var result4 = true && 'true1';
+var result5 = true && ('true1');
+
+
+//* || (OR)
+
+
+
+//* ! (NOT)
+
+
+
+console.log("1 - ", result);
+console.log("2 - ", result2);
+console.log("3 - ", result3);
+console.log("4 - ", result4);
+console.log("5 - ", result5);
