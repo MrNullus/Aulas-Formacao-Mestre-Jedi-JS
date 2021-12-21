ad54c957b3c5b9ea3dbe43a6c255eb751f2d0da2
Dev-gustavo-henrique <gustavojs417@gmail.com>
2021-12-21 19:48:30 -0300

    Create comparacao.js

---
+++ 11 - Operadores/ex/comparacao.js
@@ -0,0 +1,17 @@
+
+var operando1 = 10;
+var operando2 = 20;
+
+var resultado = operando1 == operando2;
+console.log("1 - ", resultado);
+console.log("2 - ", 10 = '10');
+console.log("3 - ", 10 === 10);
+console.log("4 - ", 1 == true);
+console.log("5 - ", 1 === true);
+console.log("6 - ", 2 != 2);
+console.log("7 - ", 2 !== '2');
+console.log("8 - ", 2 > 2);
+console.log("8 - ", 10 < 2);
+console.log("9 - ", 8 <= 12);
+console.log("10 - ", 8 >= 9);
+
